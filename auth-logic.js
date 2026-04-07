4f9925d59b1db8e3b1c99c655cb4a1bdec726acc
larpvpn <klyarskiy@gmail.com>
2026-04-07 13:25:01 +0300

    Add files via upload

--- auth-logic.js
+++ auth-logic.js
@@ -4,17 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const nav = document.getElementById('main-nav');
 
     if (user && authBtn) {
-        // Если юзер есть в памяти — меняем кнопку на ВЫХОД
         authBtn.innerText = 'ВЫХОД';
         authBtn.style.color = 'var(--error)';
         authBtn.style.borderColor = 'var(--error)';
         authBtn.onclick = (e) => {
             e.preventDefault();
-            localStorage.removeItem('userLogin'); // Удаляем только текущую сессию
+            localStorage.removeItem('userLogin');
             window.location.reload();
         };
 
-        // Добавляем ДАШБОРД в меню
         const dash = document.createElement('a');
         dash.href = 'dashboard.html';
         dash.className = 'nav-item';
